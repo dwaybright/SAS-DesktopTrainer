78061e363454ac73562d17f5fc76e72757138324
dwaybright <daniel.waybright@gmail.com>
2015-03-26 12:53:24 -0600

    Committed DesktopTrainer

--- core/src/us/thirdmillenium/desktoptrainer/DesktopTrainer.java
+++ core/src/us/thirdmillenium/desktoptrainer/DesktopTrainer.java
@@ -1,3 +1,19 @@
+/*
+ Copyright (C) 2015 Daniel Waybright, daniel.waybright@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package us.thirdmillenium.desktoptrainer;
 
 import com.badlogic.gdx.ApplicationAdapter;
@@ -10,7 +26,7 @@ import java.util.Random;
 
 
 public class DesktopTrainer extends ApplicationAdapter {
-    // Test INdex
+    // Test Index
     private int TestIndex = 3;
 
     // Environment
@@ -25,6 +41,6 @@ public class DesktopTrainer extends ApplicationAdapter {
 
     @Override
     public void render () {
-        this.MyEnvironment.render(Gdx.graphics.getDeltaTime());
+        this.MyEnvironment.simulate(Gdx.graphics.getDeltaTime());
     }
 }
