f53aeb26fadf264458b981b5b65bc6feb95e43f0
dwaybright <daniel.waybright@gmail.com>
2015-03-25 22:03:14 -0600

    Now with IntelliJ path settings

--- core/src/us/thirdmillenium/desktoptrainer/DesktopTrainer.java
+++ core/src/us/thirdmillenium/desktoptrainer/DesktopTrainer.java
@@ -13,7 +13,7 @@ public class DesktopTrainer extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("core\\assets\\badlogic.jpg");
+		img = new Texture("core/assets/badlogic.jpg");
 	}
 
 	@Override
