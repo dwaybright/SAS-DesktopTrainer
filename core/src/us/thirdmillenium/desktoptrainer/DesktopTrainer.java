c1a40a0f0ea170a3111e4e5e807080b859d626cf
dwaybright <daniel.waybright@gmail.com>
2015-03-26 11:04:30 -0600

    Environment is now an extendible Java class
    Makes using environment much more intuitive.

--- core/src/us/thirdmillenium/desktoptrainer/DesktopTrainer.java
+++ core/src/us/thirdmillenium/desktoptrainer/DesktopTrainer.java
@@ -3,24 +3,28 @@ package us.thirdmillenium.desktoptrainer;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 
-import us.thirdmillenium.desktoptrainer.environment.Environment;
+import us.thirdmillenium.desktoptrainer.environment.SinglePlayEnvironment;
 
 import java.util.Random;
 
 
 public class DesktopTrainer extends ApplicationAdapter {
+    // Test Map Index (1 - 5)
+    private int TestMapIndex = 3;
+
     // Environment
-    private Environment MyEnvironment;
+    private SinglePlayEnvironment MyEnvironment;
+
 
     @Override
     public void create () {
         Random random = new Random();
 
-        this.MyEnvironment = new Environment(TrainingParams.PathToBaseNN, random, 5);
+        this.MyEnvironment = new SinglePlayEnvironment(TrainingParams.PathToBaseNN, random, TestMapIndex);
     }
 
     @Override
     public void render () {
-        this.MyEnvironment.render(Gdx.graphics.getDeltaTime());
+        this.MyEnvironment.simulate(Gdx.graphics.getDeltaTime());
     }
 }
